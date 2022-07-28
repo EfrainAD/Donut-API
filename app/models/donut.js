c23173fbec2df7e88dbea9379d8694c1207710ad
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-28 19:59:09 -0400

    unTemplated Model

---
+++ app/models/donut.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const donutSchema = new mongoose.Schema(
+	{
+		Name: {
+			type: String,
+			required: true
+		},
+		from: {
+			type: String,
+			required: true
+		},
+		thoughts: {
+			type: String,
+		},
+		haveEaten: {
+			type: Boolean,
+			required: true
+		},
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true
+		},
+	},
+	{
+		timestamps: true
+	}
+)
+
+module.exports = mongoose.model('Donut', donutSchema)
