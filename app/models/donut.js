4d716d71a0f5a5f6eb52bc8a171e1d4b59e3946c
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-28 22:28:07 -0400

    Undid a test with id required in donut modul. Undid change I was thinking but to be consistent I need to change them everywere, so I put it back. I removed the example.js file because it ok with that now.

--- app/models/donut.js
+++ app/models/donut.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const donutSchema = new mongoose.Schema(
 	{
-		Name: {
+		name: {
 			type: String,
 			required: true
 		},
--- app/routes/donut_routes.js
+++ app/routes/donut_routes.js
@@ -3,8 +3,8 @@ const express = require('express')
 // Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
 
-// pull in Mongoose model for examples
-const Example = require('../models/example')
+// pull in Mongoose model for donuts
+const Donut = require('../models/donut')
 
 // this is a collection of methods that help us detect situations when we need
 // to throw a custom error
@@ -28,43 +28,43 @@ const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
 // INDEX
-// GET /examples
-router.get('/examples', requireToken, (req, res, next) => {
-	Example.find()
-		.then((examples) => {
-			// `examples` will be an array of Mongoose documents
+// GET /donuts
+router.get('/donuts', requireToken, (req, res, next) => {
+	Donut.find()
+		.then((donuts) => {
+			// `donuts` will be an array of Mongoose documents
 			// we want to convert each one to a POJO, so we use `.map` to
 			// apply `.toObject` to each one
-			return examples.map((example) => example.toObject())
+			return donuts.map((donut) => donut.toObject())
 		})
-		// respond with status 200 and JSON of the examples
-		.then((examples) => res.status(200).json({ examples: examples }))
+		// respond with status 200 and JSON of the donuts
+		.then((donuts) => res.status(200).json({ donuts: donuts }))
 		// if an error occurs, pass it to the handler
 		.catch(next)
 })
 
 // SHOW
-// GET /examples/5a7db6c74d55bc51bdf39793
-router.get('/examples/:id', requireToken, (req, res, next) => {
+// GET /donuts/5a7db6c74d55bc51bdf39793
+router.get('/donuts/:id', requireToken, (req, res, next) => {
 	// req.params.id will be set based on the `:id` in the route
-	Example.findById(req.params.id)
+	Donut.findById(req.params.id)
 		.then(handle404)
-		// if `findById` is succesful, respond with 200 and "example" JSON
-		.then((example) => res.status(200).json({ example: example.toObject() }))
+		// if `findById` is succesful, respond with 200 and "donut" JSON
+		.then((donut) => res.status(200).json({ donut: donut.toObject() }))
 		// if an error occurs, pass it to the handler
 		.catch(next)
 })
 
 // CREATE
-// POST /examples
-router.post('/examples', requireToken, (req, res, next) => {
-	// set owner of new example to be current user
-	req.body.example.owner = req.user.id
+// POST /donuts
+router.post('/donuts', requireToken, (req, res, next) => {
+	// set owner of new donut to be current user
+	req.body.donut.owner = req.user.id
 
-	Example.create(req.body.example)
-		// respond to succesful `create` with status 201 and JSON of new "example"
-		.then((example) => {
-			res.status(201).json({ example: example.toObject() })
+	Donut.create(req.body.donut)
+		// respond to succesful `create` with status 201 and JSON of new "donut"
+		.then((donut) => {
+			res.status(201).json({ donut: donut.toObject() })
 		})
 		// if an error occurs, pass it off to our error handler
 		// the error handler needs the error message and the `res` object so that it
@@ -73,21 +73,21 @@ router.post('/examples', requireToken, (req, res, next) => {
 })
 
 // UPDATE
-// PATCH /examples/5a7db6c74d55bc51bdf39793
-router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
+// PATCH /donuts/5a7db6c74d55bc51bdf39793
+router.patch('/donuts/:id', requireToken, removeBlanks, (req, res, next) => {
 	// if the client attempts to change the `owner` property by including a new
 	// owner, prevent that by deleting that key/value pair
-	delete req.body.example.owner
+	delete req.body.donut.owner
 
-	Example.findById(req.params.id)
+	Donut.findById(req.params.id)
 		.then(handle404)
-		.then((example) => {
+		.then((donut) => {
 			// pass the `req` object and the Mongoose record to `requireOwnership`
 			// it will throw an error if the current user isn't the owner
-			requireOwnership(req, example)
+			requireOwnership(req, donut)
 
 			// pass the result of Mongoose's `.update` to the next `.then`
-			return example.updateOne(req.body.example)
+			return donut.updateOne(req.body.donut)
 		})
 		// if that succeeded, return 204 and no JSON
 		.then(() => res.sendStatus(204))
@@ -96,15 +96,15 @@ router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
 })
 
 // DESTROY
-// DELETE /examples/5a7db6c74d55bc51bdf39793
-router.delete('/examples/:id', requireToken, (req, res, next) => {
-	Example.findById(req.params.id)
+// DELETE /donuts/5a7db6c74d55bc51bdf39793
+router.delete('/donuts/:id', requireToken, (req, res, next) => {
+	Donut.findById(req.params.id)
 		.then(handle404)
-		.then((example) => {
-			// throw an error if current user doesn't own `example`
-			requireOwnership(req, example)
-			// delete the example ONLY IF the above didn't throw
-			example.deleteOne()
+		.then((donut) => {
+			// throw an error if current user doesn't own `donut`
+			requireOwnership(req, donut)
+			// delete the donut ONLY IF the above didn't throw
+			donut.deleteOne()
 		})
 		// send back 204 and no content if the deletion succeeded
 		.then(() => res.sendStatus(204))
